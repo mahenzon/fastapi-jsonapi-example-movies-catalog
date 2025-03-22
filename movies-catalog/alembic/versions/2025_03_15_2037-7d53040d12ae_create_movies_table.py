36bb29beb4b15b7402b35700d16f33cf2fad16d4
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-22 20:36:08 +0300

    ruff lint.isort known-third-party alembic

--- movies-catalog/alembic/versions/2025_03_15_2037-7d53040d12ae_create_movies_table.py
+++ movies-catalog/alembic/versions/2025_03_15_2037-7d53040d12ae_create_movies_table.py
@@ -9,7 +9,6 @@ Create Date: 2025-03-15 20:37:33.416842
 from typing import Sequence, Union
 
 import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
--- movies-catalog/alembic/versions/2025_03_22_1815-372074b6ed46_create_age_rating_table.py
+++ movies-catalog/alembic/versions/2025_03_22_1815-372074b6ed46_create_age_rating_table.py
@@ -9,7 +9,6 @@ Create Date: 2025-03-22 18:15:48.351936
 from typing import Sequence, Union
 
 import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
