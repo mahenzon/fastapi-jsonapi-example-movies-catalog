7f0f600153626afde4af13b39978392a57f99d53
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-22 19:37:41 +0300

    add age ratings resource

--- movies-catalog/api/build_jsonapi_app.py
+++ movies-catalog/api/build_jsonapi_app.py
@@ -2,15 +2,23 @@ from fastapi import FastAPI
 from fastapi_jsonapi import ApplicationBuilder
 
 from api.generic_view import GenericView
-from models import Movie
+from models import (
+    AgeRating,
+    Movie,
+)
 from schemas import (
+    AgeRatingCreateSchema,
+    AgeRatingSchema,
+    AgeRatingUpdateSchema,
     MovieCreateSchema,
     MovieSchema,
     MovieUpdateSchema,
 )
 
 
-def build_jsonapi_app(app: FastAPI) -> ApplicationBuilder:
+def build_jsonapi_app(
+    app: FastAPI,
+) -> ApplicationBuilder:
     builder = ApplicationBuilder(
         app=app,
         prefix="/api",
@@ -25,5 +33,16 @@ def build_jsonapi_app(app: FastAPI) -> ApplicationBuilder:
         schema=MovieSchema,
         schema_in_post=MovieCreateSchema,
         schema_in_patch=MovieUpdateSchema,
+    )
+    builder.add_resource(
+        path="/age-ratings",
+        tags=["Age Ratings"],
+        resource_type="age-rating",
+        view=GenericView,
+        model=AgeRating,
+        schema=AgeRatingSchema,
+        schema_in_post=AgeRatingCreateSchema,
+        schema_in_patch=AgeRatingUpdateSchema,
+        model_id_field_name="name",
     )
     return builder
